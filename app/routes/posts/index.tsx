6ff228f04080a82de41b6bc55fc7e2d723a77543
milliorn <scottmilliorn@gmail.com>
2023-01-29 16:13:21 -0800

    Loaders

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,8 +1,39 @@
+import { Link, useLoaderData } from "@remix-run/react";
+
+export const loader = () => {
+  //console.log(123456789);
+  const data = {
+    posts: [
+      { id: 1, title: "Post 1", body: "This is a test post" },
+      { id: 2, title: "Post 2", body: "This is a test post" },
+      { id: 3, title: "Post 3", body: "This is a test post" },
+    ],
+  };
+
+  return data;
+};
+
 function PostItems(): JSX.Element {
+  const { posts } = useLoaderData();
+
   return (
-    <div>
-      <h1>PostItems</h1>
-    </div>
+    <>
+      <div className="page-header">
+        <h1>Posts</h1>
+        <Link to="/posts/new" className="btn">
+          New Post
+        </Link>
+      </div>
+      <ul className="posts-list">
+        {posts.map((post: any) => (
+          <li key={post.id}>
+            <Link to={post.id}>
+              <h3>{post.title}</h3>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
