c8dfb079243a6cc834e554c0caf8f3e27963f1a0
milliorn <scottmilliorn@gmail.com>
2023-01-29 20:39:06 -0800

    Actions

--- app/routes/posts/new.tsx
+++ app/routes/posts/new.tsx
@@ -1,7 +1,46 @@
+import type { ActionArgs } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+
+// https://remix.run/docs/en/v1/route/action#action
+export const action = async ({ request }: ActionArgs) => {
+  const form = await request.formData();
+  const title = form.get("title");
+  const body = form.get("body");
+
+  const fields = { title, body };
+
+  // console.log(fields);
+
+  return redirect("/posts");
+};
+
 export default function NewPost(): JSX.Element {
   return (
-    <div>
-      <h1>New Post</h1>
-    </div>
+    <>
+      <div className="page-header">
+        <h1>New Post</h1>
+        <Link to="/posts" className="bt btn-reverse">
+          Back
+        </Link>
+      </div>
+
+      <div className="page-content">
+        <form method="POST">
+          <div className="form-control">
+            <label htmlFor="title">Title</label>
+            <input type="text" name="title" id="title" />
+          </div>
+
+          <div className="form-control">
+            <label htmlFor="body">Post Body</label>
+            <textarea name="body" id="body" />
+          </div>
+          <button type="submit" className="btn btn-block">
+            Create Post
+          </button>
+        </form>
+      </div>
+    </>
   );
 }
